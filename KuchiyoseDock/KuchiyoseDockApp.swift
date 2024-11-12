d570c1dd2a858115c765bffc737e6fc8a7dc3be3
Raw <108172547+Thigraw@users.noreply.github.com>
2024-11-12 16:35:28 -0500

    Initial Commit

---
+++ KuchiyoseDock/KuchiyoseDockApp.swift
@@ -0,0 +1,17 @@
+//
+//  KuchiyoseDockApp.swift
+//  KuchiyoseDock
+//
+//  Created by John Yang on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct KuchiyoseDockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KuchiyoseDockTests/KuchiyoseDockTests.swift
@@ -0,0 +1,17 @@
+//
+//  KuchiyoseDockTests.swift
+//  KuchiyoseDockTests
+//
+//  Created by John Yang on 11/12/24.
+//
+
+import Testing
+@testable import KuchiyoseDock
+
+struct KuchiyoseDockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
